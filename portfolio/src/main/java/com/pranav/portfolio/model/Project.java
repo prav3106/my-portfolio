54100724ff7c91843e6598e7f50aa603f27a9510
sahulhustles <b.sahulhameed1@gmail.com>
2025-09-30 06:44:45 +0530

    commit

---
+++ portfolio/src/main/java/com/pranav/portfolio/model/Project.java
@@ -0,0 +1,13 @@
+package main.java.com.pranav.portfolio.model;
+
+
+
+@Data
+public class Project {
+  private Long id;
+  private String title;
+  private String desc;
+  private String gitLink;
+
+
+}
